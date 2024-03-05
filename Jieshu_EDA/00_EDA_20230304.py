dd8ac83ae423aa6bb4248f047e68a16cb50084d0
Jieshu <foundwonder@gmail.com>
2024-03-05 14:11:43 -0700

    Modified folder structure and visualization import

---
+++ Jieshu_EDA/00_EDA_20230304.py
@@ -0,0 +1,12 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+series_1=pd.read_csv('/Users/Jieshu/PycharmProjects/ABMSS2024/results/series_ABMSS2024_1.csv')
+
+#%%
+
+
+#%%
